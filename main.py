32db79a3b2a6c0aa09534f78efd010f597cccf12
Brandon <brandonlokemk@gmail.com>
2024-07-24 08:55:00 +0000

    changed admin id to lis

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ logger = logging.getLogger(__name__)
 
 # Define configuration constants
 URL = os.environ['CLOUD_URL']
-ADMIN_CHAT_ID = 566682368 #TODO change
+ADMIN_CHAT_ID = 494057457 #TODO change
 CHANNEL_ID = -1002192841091 #TODO change
 PORT = 8080
 BOT_TOKEN = os.environ['BOT_TOKEN'] # nosec B105
